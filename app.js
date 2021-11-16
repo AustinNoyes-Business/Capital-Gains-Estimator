53b3b3b5e7c680d511c1c59fd887c86cb04be9fb
AustinNoyes-Business <93406208+AustinNoyes-Business@users.noreply.github.com>
2021-11-15 18:59:54 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 // Display Mobile Menu
 const mobileMenu = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 };
 
-menu.addEventListener('click', mobileMenu);
+menu.addEventListener("click", mobileMenu);
+
+// const button_anim = document.getElementById('main_btn').addEventListener('click', function() {
+//   var c = document.getElementsByClassName('checkmark');
+//   for (var i = 0; i < c.length; i++) {
+//     c[i].classList.add('checkmark__check');
+//   }
+// })
